d2e3ceba2ebe611e2d7e7c59207366fce52b9bf2
John Chodera <john.chodera@choderalab.org>
2017-03-26 17:44:15 -0400

    Fix some smarty -> smirnoff stragglers

--- setup.py
+++ setup.py
@@ -13,21 +13,21 @@ def find_package_data(data_root, package_root):
     return files
 
 setup(
-    name = "smarty",
-    version = "0.1.6",
-    author = "John Chodera, David Mobley, and others",
+    name = "smirnoff",
+    version = "0.2.0",
+    author = "The Open ForceField Group",
     author_email = "john.chodera@choderalab.org",
     description = ("Automated Bayesian atomtype sampling"),
     license = "MIT",
     keywords = "Bayesian atomtype sampling forcefield parameterization",
-    url = "http://github.com/open-forcefield-group/smarty",
-    packages=['smarty', 'smarty/tests', 'smarty/data'],
+    url = "http://github.com/open-forcefield-group/smirnoff",
+    packages=['smirnoff', 'smirnoff/tests', 'smirnoff/data'],
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT",
     ],
-    entry_points={'console_scripts': ['smarty = smarty.cli_smarty:main', 'smirky = smarty.cli_smirky:main']},
-    package_data={'smarty': find_package_data('smarty/data', 'smarty')},
+    entry_points={'console_scripts': ['smirnoff = smirnoff.cli_smirnoff:main', 'smirky = smirnoff.cli_smirky:main']},
+    package_data={'smirnoff': find_package_data('smirnoff/data', 'smirnoff')},
 )
